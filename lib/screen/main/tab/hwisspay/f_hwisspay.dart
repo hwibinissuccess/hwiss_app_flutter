caf0a6d31f4e705d5314e3e1a0ccd111088e21c7
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-21 00:02:18 +0900

    feat: BottomNavigation 만들기

---
+++ lib/screen/main/tab/hwisspay/f_hwisspay.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HwisspayFragment extends StatefulWidget {
+  const HwisspayFragment({super.key});
+
+  @override
+  State<HwisspayFragment> createState() => _HwisspayFragmentState();
+}
+
+class _HwisspayFragmentState extends State<HwisspayFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
