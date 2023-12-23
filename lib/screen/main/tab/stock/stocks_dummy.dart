66840095400db23ae3d0ce721c5c300e950f1a6c
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-24 03:55:00 +0900

    feat: 주식 탭 완성

---
+++ lib/screen/main/tab/stock/stocks_dummy.dart
@@ -0,0 +1,65 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/stock/vo_stock.dart';
+
+List<Stock> myInterestStocks = [
+  Stock(
+    stockImagePath: '$basePath/stock/interest_stock_01.png',
+    stockName: '한화솔루션',
+    yesterdayClosePrice: 41600,
+    currentPrice: 41600,
+  ),
+  Stock(
+    stockImagePath: '$basePath/stock/interest_stock_02.png',
+    stockName: '현대모비스',
+    yesterdayClosePrice: 217000,
+    currentPrice: 219000,
+  ),
+  Stock(
+    stockImagePath: '$basePath/stock/interest_stock_03.png',
+    stockName: '셀트리온',
+    yesterdayClosePrice: 80000,
+    currentPrice: 70000,
+  ),
+  Stock(
+    stockImagePath: '$basePath/stock/interest_stock_04.png',
+    stockName: '하이브',
+    yesterdayClosePrice: 41600,
+    currentPrice: 41600,
+  ),
+  Stock(
+    stockImagePath: '$basePath/stock/interest_stock_05.png',
+    stockName: 'LG헬로비전',
+    yesterdayClosePrice: 80000,
+    currentPrice: 82000,
+  ),Stock(
+    stockImagePath: '$basePath/stock/interest_stock_06.png',
+    stockName: '대한전선',
+    yesterdayClosePrice: 80000,
+    currentPrice: 82000,
+  ),Stock(
+    stockImagePath: '$basePath/stock/interest_stock_07.png',
+    stockName: 'SK하이닉스',
+    yesterdayClosePrice: 80000,
+    currentPrice: 82000,
+  ),Stock(
+    stockImagePath: '$basePath/stock/interest_stock_08.png',
+    stockName: '삼성물산',
+    yesterdayClosePrice: 80000,
+    currentPrice: 92300,
+  ),Stock(
+    stockImagePath: '$basePath/stock/interest_stock_09.png',
+    stockName: '카카오',
+    yesterdayClosePrice: 80000,
+    currentPrice: 82000,
+  ),Stock(
+    stockImagePath: '$basePath/stock/interest_stock_10.png',
+    stockName: 'LG화학',
+    yesterdayClosePrice: 80000,
+    currentPrice: 82000,
+  ), Stock(
+    stockImagePath: '$basePath/stock/interest_stock_11.png',
+    stockName: '삼양옵틱스',
+    yesterdayClosePrice: 80000,
+    currentPrice: 82000,
+  ),
+];
