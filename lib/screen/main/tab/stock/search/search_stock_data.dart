7111181e7339406cac5bcdb5263759c03f9ecf6a
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-24 18:39:58 +0900

    feat : 주식 검색 자동완성

--- lib/screen/main/tab/stock/search/search_stock_data.dart
+++ lib/screen/main/tab/stock/search/search_stock_data.dart
@@ -3,23 +3,43 @@ import 'package:get/get.dart';
 
 import '../vo_simple_stock.dart';
 
-class SearchStockData extends GetxController{
+abstract mixin class SearchStockDataProvider{
+  late final searchData = Get.find<SearchStockData>();
+}
+
+class SearchStockData extends GetxController {
   List<SimpleStock> stocks = [];
   RxList<String> searchHistoryList = <String>[].obs;
   RxList<SimpleStock> autoCompleteList = <SimpleStock>[].obs;
 
   @override
   void onInit() {
-    
     searchHistoryList.addAll(['삼성전자', 'LG전자', '현대자동차', '애플']);
     loadLocalStockJson();
-    
+
     super.onInit();
   }
 
-  Future<void> loadLocalStockJson() async{
-    final jsonList = await LocalJson.getObjectList<SimpleStock>("stock_list.json");
+  Future<void> loadLocalStockJson() async {
+    final jsonList =
+        await LocalJson.getObjectList<SimpleStock>("stock_list.json");
     stocks.addAll(jsonList);
   }
 
+  void search(String keyword) {
+    if(keyword.isEmpty){
+      autoCompleteList.clear();
+      return;
+    }
+    autoCompleteList.value =
+        stocks.where((element) => element.name.contains(keyword)).toList();
+  }
+
+  void addHistory(SimpleStock stock) {
+    searchHistoryList.add(stock.name);
+  }
+
+  void removeHistory(String stockName) {
+    searchHistoryList.remove(stockName);
+  }
 }
