c641a194aa68f16d64f3f970f8456bf15a27834e
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-28 04:38:56 +0900

    feat: 스크롤에 따라 크기가 변하는 appbar

--- lib/screen/main/tab/stock/setting/s_setting.dart
+++ lib/screen/main/tab/stock/setting/s_setting.dart
@@ -2,6 +2,7 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/dart/extension/datetime_extension.dart';
 import 'package:fast_app_base/common/data/preference/app_preferences.dart';
 import 'package:fast_app_base/common/widget/w_big_button.dart';
+import 'package:fast_app_base/screen/main/tab/stock/setting/w_animated_app_bar.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/w_switch_menu.dart';
 import 'package:fast_app_base/screen/opensource/s_opensource.dart';
 import 'package:flutter/material.dart';
@@ -12,76 +13,85 @@ import 'd_number.dart';
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
+
   @override
   State<SettingScreen> createState() => _SettingScreenState();
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+
+  final  controller = ScrollController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: '설정'.text.make(),
-      ),
-      body: ListView(
+      body: Stack(
         children: [
-          // switch
-          Obx(
-            () => SwitchMenu(
-              '푸시 설정',
-              Prefs.isPushOnRx.get(),
-              onChanged: (isOn) {
-                Prefs.isPushOnRx.set(isOn);
-              },
-            ),
-          ),
+          ListView(
+            controller: controller,
+            padding: const EdgeInsets.only(top: 150),
+            children: [
+              // switch
+              Obx(
+                () => SwitchMenu(
+                  '푸시 설정',
+                  Prefs.isPushOnRx.get(),
+                  onChanged: (isOn) {
+                    Prefs.isPushOnRx.set(isOn);
+                  },
+                ),
+              ),
 
-          //slider
-          Obx(
-            () => Slider(
-              value: Prefs.sliderPosition.get(),
-              onChanged: (value) {
-                Prefs.sliderPosition.set(value);
-              },
-            ),
-          ),
-          //date time
-          Obx(
-            () => BigButton(
-              '날짜 ${Prefs.birthday.get() == null ? "" : Prefs.birthday.get()?.formattedTime}',
-              onTap: () async {
-                final date = await showDatePicker(
-                  context: context,
-                  firstDate: DateTime.now().subtract(90.days),
-                  lastDate: DateTime.now().add(90.days),
-                  initialDate: DateTime.now(),
-                );
-                if (date != null) {
-                  Prefs.birthday.set(date);
-                }
-              },
-            ),
-          ),
+              //slider
+              Obx(
+                () => Slider(
+                  value: Prefs.sliderPosition.get(),
+                  onChanged: (value) {
+                    Prefs.sliderPosition.set(value);
+                  },
+                ),
+              ),
+              //date time
+              Obx(
+                () => BigButton(
+                  '날짜 ${Prefs.birthday.get() == null ? "" : Prefs.birthday.get()?.formattedTime}',
+                  onTap: () async {
+                    final date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now().subtract(90.days),
+                      lastDate: DateTime.now().add(90.days),
+                      initialDate: DateTime.now(),
+                    );
+                    if (date != null) {
+                      Prefs.birthday.set(date);
+                    }
+                  },
+                ),
+              ),
 
-          //number dialog
-          Obx(
-            () => BigButton(
-              '저장된 숫자 ${Prefs.number.get()}',
-              onTap: () async {
-                final number = await NumberDialog().show();
-                if (number != null) {
-                  Prefs.number.set(number);
-                }
-              },
-            ),
-          ),
+              //number dialog
+              Obx(
+                () => BigButton(
+                  '저장된 숫자 ${Prefs.number.get()}',
+                  onTap: () async {
+                    final number = await NumberDialog().show();
+                    if (number != null) {
+                      Prefs.number.set(number);
+                    }
+                  },
+                ),
+              ),
 
-          BigButton(
-            '오픈소스화면',
-            onTap: () async {
-              Nav.push(OpensourceScreen());
-            },
+              BigButton(
+                '오픈소스화면',
+                onTap: () async {
+                  Nav.push(OpensourceScreen());
+                },
+              ),
+            ],
           ),
+          AnimatedAppBar('설정',controller: controller)
         ],
       ),
     );
